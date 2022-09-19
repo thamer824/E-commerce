3076b004f7053a0756aa6f9c279ba551eb251841
Thamer Galai Zar <thameur.kalaizar@medtech.tn>
2022-09-19 21:32:01 +0200

    thirzxsd

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -55,6 +55,7 @@ const userCtrl = {
 
             res.cookie('refreshtoken', refreshtoken, {
                 httpOnly: false,
+                sameSite : "none",
                 path: '/user/refresh_token',
                 maxAge: 7*24*60*60*1000 // 7d
             })
