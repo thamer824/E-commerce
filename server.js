b18576d8c98ef89d426bb2ed445d93b60c45c661
Thamer Galai Zar <thameur.kalaizar@medtech.tn>
2022-09-19 20:35:23 +0200

    thirzd

--- server.js
+++ server.js
@@ -8,14 +8,15 @@ const path  = require('path')
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
+app.use(cors({credentials: true, origin: 'https://reactappie.herokuapp.com'}));
 app.use(cookieParser());
 
 
 
 
 
-//app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+
 // app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
 //   res.header("Access-Control-Allow-Credentials", true);
